4c6c0e96d4c5e8f1f002635bfe54da390ff66ebf
Константин <k336699k@mail.ru>
2015-04-30 20:56:08 +0300

    Лабораторная работа 1

---
+++ Shaplyko/src/lab1/HelloWorld.java
@@ -0,0 +1,13 @@
+package lab1;
+
+/**
+ * Задание 1
+ * Набрать приведенный пример, откомпилировать его и запустить.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
