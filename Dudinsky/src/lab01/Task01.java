62fc99661bcc9553097fb703e65b79a9b7c4f3d4
dudinsky <dudinsky@tut.by>
2015-05-01 15:22:59 +0300

    Task01

---
+++ Dudinsky/src/lab01/Task01.java
@@ -0,0 +1,8 @@
+package lab01;
+
+/* Задание 1. Набрать приведенный пример, откомпилировать его и запустить. */
+public class Task01 {
+    public static void main(String[] args) {
+        System.out.println("Have a nice day!");
+    }
+}
